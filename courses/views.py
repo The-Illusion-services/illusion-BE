eec76980253506841acda82f8f29267d7db05b00
Noah <noahtochukwu10@gmail.com>
2024-09-20 02:58:24 +0100

    added the create/list logic of the course creation to the app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from serializers.serializers import *
+from permissions.permissions import IsEmployer 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+
+class CourseListAndCreate(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsEmployer]
+
+    def perform_create(self, serializer):
+        # Pass the request user to the serializer
+        serializer.save(created_by=self.request.user)
