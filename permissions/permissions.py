63b6633234e5adef5b4079fb950b39a7d5f9f823
Noah <noahtochukwu10@gmail.com>
2024-09-22 17:00:26 +0100

    added IsEmployee permission

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -4,3 +4,7 @@ class IsEmployer(BasePermission):
     def has_permission(self, request, view):
         # Check if the user is authenticated and their role is 'Employer'
         return request.user.is_authenticated and request.user.role == 'Employer'
+
+class IsEmployee(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'Employee'
